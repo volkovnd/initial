7709a0284c6e1b415854853c4a28ee2aa4fcec23
Volkov Nikolay <volkovnd@yandex.ru>
2021-10-24 21:47:29 +0300

    Added initial @vue/cli configs

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,40 @@
+/** @type {import("@vue/cli-service").ProjectOptions} */
 module.exports = {
   lintOnSave: false,
+  productionSourceMap: false,
+
+  css: {
+    sourceMap: process.env.NODE_ENV !== "production",
+  },
+
+  chainWebpack: (config) => {
+    config.mode = "development";
+  },
+
+  configureWebpack: (config) => {
+    config.devtool = config.mode === "development" ? "source-map" : false;
+  },
+};
+
+/** @type {import("webpack-dev-server").Configuration} */
+module.exports.devServer = {
+  host: process.env.HOST || "0.0.0.0",
+  port: process.env.PORT || 8080,
+  useLocalIp: true,
+  open: true,
+  compress: true,
+  transportMode: {
+    client: "ws",
+    server: "ws",
+  },
+  clientLogLevel: "none",
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  },
+  hot: true,
+  noInfo: true,
+
+  before: (app) => {
+    require("./devServer")(app);
+  },
 };
