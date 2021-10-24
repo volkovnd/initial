2f1080c9bebd9a5a275be80d3c99f5b9973ff782
Volkov Nikolay <volkovnd@yandex.ru>
2021-10-25 00:26:41 +0300

    Added reset.scss

--- src/App.vue
+++ src/App.vue
@@ -11,25 +11,5 @@ export default {
 </script>
 
 <style lang="scss">
-@import "~normalize.css/normalize.css";
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-}
-
-body {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-h1,
-h2,
-h3,
-h4,
-h5,
-h6 {
-  margin-top: 0;
-}
+@import "~@/styles/reset";
 </style>
