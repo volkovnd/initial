8b5bd76999828348866c4e544228d144c3a3d8e2
Volkov Nikolay <volkovnd@yandex.ru>
2021-10-25 15:27:48 +0300

    Placed setting to enable JSON server in .env

--- createJSONServer.js
+++
@@ -1,7 +0,0 @@
-const createJSONServer = require("@vue/cli-test-utils/createJSONServer.js");
-const db = require("./tests/fixtures");
-
-module.exports = (app) => {
-  app.use("/api", createJSONServer(db));
-};
-
--- devServer.js
+++ devServer.js
@@ -3,5 +3,10 @@ const express = require("express");
 module.exports = (app) => {
   app.use(express.json());
 
-  require("./createJSONServer")(app);
+  if (process.env.CREATE_JSON_SERVER === true || process.env.CREATE_JSON_SERVER === "true") {
+    const createJSONServer = require("@vue/cli-test-utils/createJSONServer.js");
+    const db = require("./tests/fixtures");
+
+    app.use("/api", createJSONServer(db));
+  }
 };
