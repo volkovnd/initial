eb264dbfa1336996c5a2832c8960b3838cd21e0b
Volkov Nikolay <volkovnd@yandex.ru>
2021-11-08 21:00:31 +0300

    Added btn component

---
+++ src/components/btn/v-btn.vue
@@ -0,0 +1,42 @@
+<script>
+import { mergeData } from "vue-functional-data-merge";
+
+export default {
+  name: "VBtn",
+  functional: true,
+  props: {
+    tagName: {
+      type: String,
+      default: "button",
+    },
+    type: {
+      type: String,
+      default: "type",
+    },
+    variant: {
+      type: String,
+      default: null,
+    },
+    size: {
+      type: String,
+      default: null,
+    },
+  },
+  render: (h, { data, props, children }) => {
+    return h(
+      props.tagName,
+      mergeData(data, {
+        attrs: {
+          type: props.type,
+        },
+        staticClass: "btn",
+        class: {
+          [`btn-${props.variant}`]: !!props.variant,
+          [`btn-${props.size}`]: !!props.size,
+        },
+      }),
+      children
+    );
+  },
+};
+</script>
