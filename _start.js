876556c497dca3b19d6f348a188fdf46f3f75f9b
Volkov Nikolay <volkovnd@yandex.ru>
2021-11-09 00:55:37 +0300

    Added script for initial generating

---
+++ _start.js
@@ -0,0 +1,60 @@
+const _ = require("lodash");
+const path = require("path");
+const fs = require("fs");
+
+/**
+ *
+ * @param  {...string} pathSegments
+ * @returns {string}
+ */
+function resolve(...pathSegments) {
+  return path.resolve(__dirname, ...pathSegments);
+}
+
+/**
+ *
+ * @param {string} path
+ * @param {string} [encoding='utf-8']
+ * @returns {string}
+ */
+function readFile(path, encoding = "utf-8") {
+  return fs.readFileSync(path, { encoding: encoding });
+}
+
+/**
+ *
+ * @param {string} fileName
+ * @param {(output: string) => string} fn
+ */
+function replaceFile(fileName, fn) {
+  const p = resolve(fileName);
+
+  const output = fn(readFile(p));
+
+  fs.writeFileSync(p, output);
+}
+
+/**
+ *
+ * @param {string} [mode='development']
+ * @param {boolean} [local=false]
+ */
+function createEnv(mode = "development", local = false) {
+  const output = readFile(resolve(".env.example"));
+
+  fs.writeFileSync(resolve(`.env.${mode}${local ? ".local" : ""}`), output);
+}
+
+const projectTitle = "Test title";
+
+replaceFile("package.json", (str) =>
+  str.replace(/"name": "[\w/@-_]+",\n/, `"name": "${_.kebabCase(projectTitle)}",\n`)
+);
+
+// Replace title in .env.example to correct
+replaceFile(".env.example", (str) => str.replace(/VUE_APP_TITLE=[\w -_]+\n/, `VUE_APP_TITLE=${projectTitle}\n`));
+
+// Replace title in README.md to correct
+replaceFile("README.md", (str) => str.replace(/#[\w -_]+\n/, `# ${projectTitle}\n`));
+
+createEnv("development", true);
